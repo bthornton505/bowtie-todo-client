82dea0e526aab461a005faa09e7f4ee1a90fee03
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-28 19:17:32 -0400

    created components folder to hold UI components

---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return(
+      <div className="border border-secondary p-4">
+        <h2>Projects</h2>
+        <hr></hr>
+        <p>
+          Create todo app
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Projects;
