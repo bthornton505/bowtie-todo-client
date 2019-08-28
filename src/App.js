f179317416032de737af70f9760162a8be1ec749
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-28 19:17:13 -0400

    added basic bootstrap layout css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
+    <div className="container text-center w-50 p-3">
+      <h1 className="p-3">
+        Todo App
+      </h1>
+
+      <Projects />
+
     </div>
   );
 }
