9bec69baf0669ca6bf6c50798b2c21ede9907a08
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-29 15:28:43 -0400

    added AddTodo component and added an edit button

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import Button from './Button';
+import AddTodo from './AddTodo'
 
 const ProjectDetails = () => {
-  // const { project } = this.props.match.params
-
   return(
     <div className="border border-secondary p-4 rounded-lg">
-      <h2 className="p-3">Project Name</h2>
+      <h2 className="text-center p-3">Project Name</h2>
 
-      <ul className="list-group list-group-flush">
+      <AddTodo />
+
+      <ul className="text-center list-group list-group-flush">
         <li className="list-group-item list-group-item-action">todo #1</li>
         <li className="list-group-item list-group-item-action">todo #2</li>
         <li className="list-group-item list-group-item-action">todo #3</li>
       </ul>
+
+      <div className="flex-row text-center pt-4">
+        <Button path={'/'} buttonText={'Back'}/>
+        <Button path={'/project/:name/edit'} buttonText={'Edit'}/>
+      </div>
     </div>
   )
 }
