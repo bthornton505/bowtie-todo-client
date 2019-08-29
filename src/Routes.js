b0f7dc47a5a9c826109862ee07e83093a0b7fb2a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-29 17:09:34 -0400

    added routes for when authentication is possible

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Register from './authentication/Register';
+import Login from './authentication/Login';
 import ProjectsContainer from './components/ProjectsContainer';
 import ProjectForm from './components/ProjectForm';
 import ProjectDetails from './components/ProjectDetails';
 
 const Routes = () => {
 
+  const guestViews = (
+    <Switch>
+      <Route exact path="/signup" component={Register} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  )
+
   const userViews = (
     <Switch>
+      <Route exact path="/signup" component={Register} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/" component={ProjectsContainer} />
       <Route exact path="/project/new" component={ProjectForm} />
       <Route exact path="/project/:name" component={ProjectDetails}/>
