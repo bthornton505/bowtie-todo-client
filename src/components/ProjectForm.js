e8ec4a58a06c2c351adaafe59fd765fe872f6745
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-29 15:31:01 -0400

    created a form for creating new projects

---
+++ src/components/ProjectForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Button from './Button';
+
+class ProjectForm extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      title: "",
+      isCreated: false
+    }
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const project = this.state
+
+    fetch('server', {
+      method: "POST",
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify( project )
+    })
+    .then(response => response.json())
+    .catch(error => console.log(error))
+
+    this.setState({
+      title: "",
+      isCreated: true
+    })
+  }
+
+  render(){
+    return(
+      <div className="border border-secondary p-4 rounded-lg">
+        <h2 className="text-center p-3">New Project</h2>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput">Project Title</label>
+            <input
+              name="title"
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput"
+              placeholder="Add Title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Button path={'/'} buttonText={'Back'}/>
+        </form>
+
+      </div>
+    )
+  }
+}
+
+export default ProjectForm;
