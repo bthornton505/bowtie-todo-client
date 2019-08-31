e3aa4998e7fb6ea095788651a3a44705318cb4e0
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-31 12:24:35 -0400

    added a new route for the landing page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import LandingPage from './components/LandingPage';
 import Register from './authentication/Register';
 import Login from './authentication/Login';
 import ProjectsContainer from './components/ProjectsContainer';
@@ -11,6 +12,7 @@ const Routes = () => {
 
   const guestViews = (
     <Switch>
+      <Route exact path="/" component={LandingPage} />
       <Route exact path="/signup" component={Register} />
       <Route exact path="/login" component={Login} />
     </Switch>
@@ -18,9 +20,10 @@ const Routes = () => {
 
   const userViews = (
     <Switch>
+      <Route exact path="/" component={LandingPage} />
       <Route exact path="/signup" component={Register} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/" component={ProjectsContainer} />
+      <Route exact path="/projects" component={ProjectsContainer} />
       <Route exact path="/project/new" component={ProjectForm} />
       <Route exact path="/project/:name" component={ProjectDetails}/>
     </Switch>
