54453812a1ff7fa9de52f6bcd8e823a9dbbec6c7
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-02 23:57:26 -0400

    created fetch request to post a new project and added a redirect after it's created

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Button from './Button';
+import { Redirect } from 'react-router-dom';
+import API_URL from '../fetchRequests/apiUrl';
 
 class ProjectForm extends Component {
   constructor(props){
@@ -22,28 +24,34 @@ class ProjectForm extends Component {
     event.preventDefault()
     const project = this.state
 
-    fetch('server', {
+    fetch(`${API_URL}/api/v1/projects`, {
       method: "POST",
       headers: {
-        "Content-Type": 'application/json'
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.auth_token}`
       },
       body: JSON.stringify( project )
     })
     .then(response => response.json())
-    .catch(error => console.log(error))
-
-    this.setState({
+    .then(() => this.setState({
       title: "",
       isCreated: true
-    })
+    }))
+    .catch(error => console.log(error))
   }
 
   render(){
+    const { isCreated } = this.state
+
+    if (isCreated === true) {
+      return <Redirect to="/projects" />
+    }
+
     return(
       <div className="border border-secondary p-4 rounded-lg">
         <h2 className="text-center p-3">New Project</h2>
         <hr></hr>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="formGroupExampleInput">Project Title</label>
             <input
