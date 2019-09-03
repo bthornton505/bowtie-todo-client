125dc53e511d9c2d5346a1e5945a6a023f664b4e
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-03 12:09:53 -0400

    created component for rendering a project but probably won't need it

---
+++ src/components/Project.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ProjectDetails from './ProjectDetails';
+
+class Project extends Component {
+
+  // componentDidMount = () => {
+  //   const projectId
+  // }
+
+  render() {
+    return(
+      <div>
+        <ProjectDetails />
+      </div>
+    )
+  }
+}
+
+export default Project;
