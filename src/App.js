4f8aec00e657c290897e1cda6abb97dd7e32be53
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-02 22:40:57 -0400

    removed state and created variables to pass as props for authenticated user

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import Routes from './Routes'
 import { checkToken } from './fetchRequests/requests'
 
 class App extends Component {
-  state = {
-    user: {},
-    isAuthenticated: false
-  }
 
-  componentDidMount = () => {
-    const authToken = localStorage.auth_token
-    console.log(authToken)
-
-    if (authToken) {
-      return checkToken(authToken)
-    }
-  }
+  // componentWillMount = () => {
+  //   const authToken = localStorage.auth_token
+  //   const { isAuthenticated } = this.state
+  //
+  //   if (isAuthenticated !== undefined) {
+  //     checkToken(authToken)
+  //   } else {
+  //     this.history.push('/login')
+  //   }
+  // }
 
   render(){
+    // const { user, userId, isAuthenticated } = this.state
+    const isAuthenticated = localStorage.getItem('auth_token')
+    const user = localStorage.getItem('user')
+    const userId = localStorage.getItem('userId')
+
     return (
       <div className="container w-50 p-3">
         <h1 className="text-center p-3">
           To-do App
         </h1>
 
-        <Routes user={this.state.user} isAuthenticated={this.state.isAuthenticated}/>
+        <Routes
+          isAuthenticated={isAuthenticated}
+          user={user}
+          userId={userId}
+        />
 
       </div>
     );
