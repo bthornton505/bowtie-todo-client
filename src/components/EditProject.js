7bbdba719dafdd77d754b0ab9767538e911cc637
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-03 12:09:24 -0400

    created component for editing a project, but isn't done yet

---
+++ src/components/EditProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditProject extends Component {
+  render(){
+    return(
+      <div className="border border-secondary p-4 rounded-lg">
+        <h2 className="text-center p-3">Edit Project</h2>
+      </div>
+    )
+  }
+}
+
+export default EditProject;
