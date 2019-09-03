11d9fd8e27919d2b956e0abd4abb04eb7fef87e2
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-03 12:05:30 -0400

    added a key attribute for each li tag and added state to each Link for passing the project

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom'
 const AllProjects = (props) => {
    const projectList = props.projects.map(project => {
      return(
-       <li className="list-group-item list-group-item-action">
+       <li key={project.id} className="list-group-item list-group-item-action">
         <Link to={{
-          pathname: `/project/${project.title}`
+          pathname: `/project/${project.title}`,
+          state: {
+            project: project
+          }
         }}>
           {project.title}
         </Link>
