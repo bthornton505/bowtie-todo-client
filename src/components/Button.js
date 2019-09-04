c4911d2c67eac10463e665c4aae42b8afc682561
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-03 23:23:59 -0400

    added state attribute to the link but might remove it

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Button = (props) => {
   return(
-    <Link to={{ pathname: props.path }}
+    <Link to={{
+      pathname: props.path,
+      state: {
+        project: props.state
+      }
+    }}
       className="text-center m-2 btn btn-primary"
       role="button"
       aria-pressed="true">
