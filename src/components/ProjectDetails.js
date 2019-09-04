3e8e7eed1def37c848fd279eee1a02170a88343a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-03 23:26:16 -0400

    made this into class component which fetchs the recipe and added delete functionality to this component

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,28 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Button from './Button';
 import AddTodo from './AddTodo'
+import API_URL from '../fetchRequests/apiUrl';
 
-const ProjectDetails = (props) => {
-  const { project } = props.location.state
-  console.log(project)
-  return(
-    <div className="border border-secondary p-4 rounded-lg">
-      <h2 className="text-center p-3">{project.title}</h2>
+class ProjectDetails extends Component {
+  state = {
+    project: [],
+    projectDeleted: false
+  }
 
-      <AddTodo />
+  componentDidMount = () => {
+    const projectId = this.props.match.params.id
 
-      <ul className="text-center list-group list-group-flush">
-        <li className="list-group-item list-group-item-action">todo #1</li>
-        <li className="list-group-item list-group-item-action">todo #2</li>
-        <li className="list-group-item list-group-item-action">todo #3</li>
-      </ul>
+    fetch(`${API_URL}/api/v1/projects/${projectId}`, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer ${localStorage.auth_token}`
+      }
+    })
+    .then(response => response.json())
+    .then(project => this.setState({
+      project: project
+    }))
+    .catch(error => console.log(error))
+  }
 
-      <div className="flex-row text-center pt-4">
-        <Button path={'/projects'} buttonText={'Back'}/>
-        <Button path={'/project/:title/edit'} buttonText={'Edit'}/>
+  deleteProject = (event) => {
+    const { project } = this.state
+
+    window.alert('Do you want to delete this project?')
+
+    fetch(`${API_URL}/api/v1/projects/${project.id}`, {
+      method: "DELETE",
+      headers: {
+        "Accept": "application/json",
+        "Authorization": `Bearer ${localStorage.auth_token}`
+      }
+    })
+    .then(response => response.json())
+    .then(() => {
+      this.setState({
+        projectDeleted: true
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  render(){
+    const { project, projectDeleted } = this.state
+
+    if (projectDeleted === true){
+      return <Redirect to="/projects" />
+    }
+
+    return(
+      <div className="border border-secondary p-4 rounded-lg">
+        <h2 className="text-center p-3">{project.title}</h2>
+
+        <AddTodo />
+
+        <ul className="text-center list-group list-group-flush">
+          <li className="list-group-item list-group-item-action">todo #1</li>
+          <li className="list-group-item list-group-item-action">todo #2</li>
+          <li className="list-group-item list-group-item-action">todo #3</li>
+        </ul>
+
+        <div className="flex-row text-center pt-4">
+          <Button path={'/projects'} buttonText={'Back'}/>
+          <Button path={`/project/${project.id}/edit`}
+            state={project}
+            buttonText={'Edit'}
+          />
+          <button type="submit" className="m-2 btn btn-primary" onClick={this.deleteProject}>
+            Delete
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ProjectDetails;
