cc86cac8579675f7c2ce07045ec05bdd2bd2b0a2
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-05 20:39:29 -0600

    added event handle functions on the delete and checkbox features

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,13 @@ import React from 'react';
 const Todo = (props) => {
   return(
     <li key={props.todo.id} className="list-group-item">
-      <input type="checkbox" className="form-check-input pull-left"/>
+      <input type="checkbox" checked={props.todo.completed} onChange={() => props.updateTodo(props.todo.id)} className="form-check-input pull-left"/>
       <span className="name">{props.todo.title}</span>
-      <button className="btn btn-danger btn-xs pull-right close" type="submit">
+      <button
+        className="btn btn-danger btn-xs pull-right close"
+        type="submit"
+        onClick={() => props.removeProjectTodos(props.todo.id)}
+      >
         x
       </button>
     </li>
